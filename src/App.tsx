d981875d526163e4d0983e9aa0f196dbab8da7a4
jin <jin@cb2f2117-a00f-45f1-b23b-36bc27fbaeb1>
2024-03-26 03:31:27 +0000

    c
    git-svn-id: http://153.127.249.127/repos/base/web/template@687 cb2f2117-a00f-45f1-b23b-36bc27fbaeb1

--- src/App.tsx
+++ src/App.tsx
@@ -186,7 +186,7 @@ export default function App(): JSX.Element {
 	return (
 		<RouterProvider
 			router={createBrowserRouter(router, {
-				// basename: "/",
+				basename: "/",
 				future: {
 					v7_fetcherPersist: true,
 					v7_normalizeFormMethod: true,
