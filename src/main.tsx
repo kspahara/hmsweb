04ac31c4bb2eb5909cd49ce1f259ada82d037954
jin <jin@cb2f2117-a00f-45f1-b23b-36bc27fbaeb1>
2024-04-05 04:18:46 +0000

    c
    git-svn-id: http://153.127.249.127/repos/base/web/template@702 cb2f2117-a00f-45f1-b23b-36bc27fbaeb1

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-// import "./assets/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "./app/assets/index.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
